c4002424761db03a2c564a60436a4466f2128180
Your Name <your.email@example.com>
2026-07-06 22:59:44 +0700

    update

--- bot.js
+++ bot.js
@@ -46,18 +46,18 @@ const userStatus = {};
 let delayLoop = 480000;
 
 async function main() {
-    await client.start({
-        phoneNumber: async () => await input.text("Nomor: "),
-        password: async () => await input.text("Password: "),
-        phoneCode: async () => await input.text("OTP: "),
-    });
-    console.log(client.session.save());
+    // await client.start({
+    //     phoneNumber: async () => await input.text("Nomor: "),
+    //     password: async () => await input.text("Password: "),
+    //     phoneCode: async () => await input.text("OTP: "),
+    // });
+    // console.log(client.session.save());
     console.log("Session exists:", fs.existsSync("session.txt"));
     console.log("Session length:", sessionData.length);
 
-    // await client.connect();
-    // console.log("Client connected 🚀");
-    // // console.log("Me:", await client.getMe());
+    await client.connect();
+    console.log("Client connected 🚀");
+    // console.log("Me:", await client.getMe());
 
     console.log("Login berhasil!");
 
