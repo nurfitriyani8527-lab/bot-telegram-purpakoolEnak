6eabc073b1a6d8a79548c7a3e09ac4efab7214ce
Your Name <your.email@example.com>
2026-08-22 18:37:01 +0700

    delete groups

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ const client = new TelegramClient(
 const groups = [
     "adoptmeindooooo",
     "adoptmeindosuper",
-    "lpmAdoptmeindon"
+    // "lpmAdoptmeindon"
 ];
 
 const delay = (ms) => new Promise(res => setTimeout(res, ms));
